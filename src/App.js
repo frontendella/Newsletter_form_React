45ac88cdb366ab912a96334b74030a5a41c6fe57
Ella R <ryukhinaella@gmail.com>
2022-11-19 16:30:28 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react'
 
 function App() {
+
+  const [enteredData, setEnteredData] =
+    useState({
+      email: "",
+      password: "",
+      passwordConfirmation: "",
+      okayToEmail: false
+    })
+
+
+
+
+  function handleChange(event) {
+    const { name, value, type, checked } = event.target
+    setEnteredData(prevEnteredData => ({
+      ...prevEnteredData,
+      [name]: type === "checkbox" ? checked : value
+    }))
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    enteredData.password === enteredData.passwordConfirmation
+      ? console.log("Successfully signed up") &&
+      enteredData.okayToEmail && console.log("Thanks for signing up for our newsletter!")
+      : console.log("Passwords to not match")
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="form-container">
+      <form className="form" onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Email address"
+          className="form--input"
+          onChange={handleChange}
+          name="email"
+          value={enteredData.email}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="form--input"
+          onChange={handleChange}
+          name="password"
+          value={enteredData.password}
+        />
+        <input
+          type="password"
+          placeholder="Confirm password"
+          className="form--input"
+          onChange={handleChange}
+          name="passwordConfirmation"
+          value={enteredData.passwordConfirmation}
+        />
+
+        <div className="form--marketing">
+          <input
+            id="okayToEmail"
+            type="checkbox"
+            onChange={handleChange}
+            name="okayToEmail"
+            checked={enteredData.okayToEmail}
+          />
+          <label htmlFor="okayToEmail">I want to join the newsletter</label>
+        </div>
+        <button
+          className="form--submit"
         >
-          Learn React
-        </a>
-      </header>
+          Sign up
+        </button>
+      </form>
     </div>
-  );
+  )
 }
 
 export default App;
